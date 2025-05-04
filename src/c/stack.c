dfa5e9e56cf14a96990e9d3c08493ba6b77f31ec
Hwiyeahh <fira.berlianti@gmail.com>
2025-05-04 20:08:05 +0700

    fix: fix output di prosedur printStackObat

--- src/c/stack.c
+++ src/c/stack.c
@@ -4,21 +4,23 @@
 void CreateEmptyStack(Stack *S){
     S->top=-1;
 }
+
 boolean isStackEmpty(Stack S){
     return S.top==-1;
 }
+
 boolean isStackFull(Stack S){
     return S.top==MAX_STACK_SIZE-1;
 }
 
 void printStackObat(Stack S){
-    for(int i=0; i<S.top; i++){
+    for(int i=0; i<=S.top; i++){
         printf("%s", S.obat[i]);
-        if(i<S.top-1){
+        if(i<S.top){
             printf(" -> ");
         }
-        printf("\n");
     }
+    printf("\n");
 }
 
 boolean isStackEqual(Stack S1, Stack S2){
@@ -39,6 +41,7 @@ void push(Stack *S, Obat val){
         strncpy(S->obat[S->top], val, MAX_OBAT_LENGTH);
     }
 }
+
 void pop(Stack *S, Obat *val){
     if(!isStackEmpty(*S)){
         strncpy(*val, S->obat[S->top], MAX_OBAT_LENGTH);

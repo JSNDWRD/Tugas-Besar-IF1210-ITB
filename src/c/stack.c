f018f71ddd92261566dd52e5295e9e59b7b483c5
Hwiyeahh <fira.berlianti@gmail.com>
2025-05-20 23:39:35 +0700

    refactor: rename functional, add comment

--- src/c/stack.c
+++ src/c/stack.c
@@ -1,50 +1,37 @@
 #include "../header/stack.h"
-#include "../header/kamus.h"
 
-void CreateEmptyStack(Stack *S){
-    S->top=-1;
+void CreateEmptyStack(Stack *s){
+    s->top=-1;
 }
 
-boolean isStackEmpty(Stack S){
-    return S.top==-1;
+bool IsStackEmpty(Stack s){
+    return s.top==-1;
 }
 
-boolean isStackFull(Stack S){
-    return S.top==MAX_STACK_SIZE-1;
+bool IsStackFull(Stack s){
+    return s.top==MAX_STACK_SIZE-1;
 }
 
-void printStackObat(Stack S){
-    for(int i=0; i<=S.top; i++){
-        printf("%s", S.obat[i]);
-        if(i<S.top){
+void PrintStackObat(Stack s){
+    for(int i=0; i<=s.top; i++){
+        printf("%s", s.obat[i]);
+        if(i<s.top){
             printf(" -> ");
         }
     }
     printf("\n");
 }
 
-boolean isStackEqual(Stack S1, Stack S2){
-    if(S1.top!=S2.top){
-        return false;
-    }
-    for(int i=0; i<S1.top; i++){
-        if(strcmp(S1.obat[i],S2.obat[i])!=0){
-            return false;
-        }
-    }
-    return true;
-}
-
-void push(Stack *S, Obat val){
-    if(!isStackFull(*S)){
-        S->top++;
-        strncpy(S->obat[S->top], val, MAX_OBAT_LENGTH);
+void Push(Stack *s, Obat val){
+    if(!isStackFull(*s)){
+        s->top++;
+        strncpy(s->obat[s->top], val, MAX_OBAT_LENGTH);
     }
 }
 
-void pop(Stack *S, Obat *val){
-    if(!isStackEmpty(*S)){
-        strncpy(*val, S->obat[S->top], MAX_OBAT_LENGTH);
-        S->top--;
+void Pop(Stack *s, Obat *val){
+    if(!isStackEmpty(*s)){
+        strncpy(*val, s->obat[s->top], MAX_OBAT_LENGTH);
+        s->top--;
     }
 }
--- src/header/stack.h
+++ src/header/stack.h
@@ -1,16 +1,36 @@
 #ifndef STACK_H
 #define STACK_H
 
-#include "kamus.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-void CreateEmptyStack(Stack *S);
-boolean isStackEmpty(Stack S);
-boolean isStackFull(Stack S);
-void printStackObat(Stack S);
-boolean isStackEqual(Stack S1, Stack S2);
-void push(Stack *S, Obat val);
-void pop(Stack *S, Obat *val);
+#define MAX_OBAT_LENGTH 50
+#define MAX_STACK_SIZE 100
+
+typedef char Obat[MAX_OBAT_LENGTH];
+
+typedef struct { 
+    Obat obat[MAX_STACK_SIZE]; 
+    int top;
+} Stack;
+
+/* Membuat stack kosong */
+void CreateEmptyStack(Stack *s);
+
+/* Mengembalikan true jika stack kosong */
+bool IsStackEmpty(Stack s);
+
+/* Mengembalikan true jika stack penuh */
+bool IsStackFull(Stack s);
+
+/* Print stack obat (yang berada di perut) */
+void PrintStackObat(Stack s);
+
+/* Menambahkan val sebagai elemen top */
+void Push(Stack *s, Obat val);
+
+/* Mengambil nilai elemen top, sehingga top yang baru adalah elemen yang datang sebelum elemen top */
+void Pop(Stack *s, Obat *val);
 
 #endif

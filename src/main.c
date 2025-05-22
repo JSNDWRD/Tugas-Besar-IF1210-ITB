23edeb378630efc5aac8cd6223b756d0cd8b583f
JSNDWRD <jasonedwardsalim@gmail.com>
2025-05-23 01:31:41 +0700

    feat: integrate ANTRIAN (F15) to main

--- src/main.c
+++ src/main.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
     CommandList commandList; // Daftar command yang dapat digunakan
 
     const char *COMMAND_READY[COMMAND_CAPACITY] = {
-        "HELP", "LOGIN", "LOGOUT", "REGISTER", "EXIT", "LUPA_PASSWORD", "LIHAT_USER", "LIHAT_PASIEN", "LIHAT_DOKTER", "CARI_USER", "CARI_PASIEN", "CARI_DOKTER", "TAMBAH_DOKTER", "LIHAT_DENAH", "LIHAT_RUANGAN", "ASSIGN_DOKTER", "DIAGNOSIS", "NGOBATIN", "LIHAT_SEMUA_ANTRIAN", "SAVE", "DAFTAR_CHECKUP"};
+        "HELP", "LOGIN", "LOGOUT", "REGISTER", "EXIT", "LUPA_PASSWORD", "LIHAT_USER", "LIHAT_PASIEN", "LIHAT_DOKTER", "CARI_USER", "CARI_PASIEN", "CARI_DOKTER", "TAMBAH_DOKTER", "LIHAT_DENAH", "LIHAT_RUANGAN", "ASSIGN_DOKTER", "DIAGNOSIS", "NGOBATIN", "LIHAT_SEMUA_ANTRIAN", "SAVE", "DAFTAR_CHECKUP", "ANTRIAN"};
     enum Command
     {
         HELP = 1,
@@ -55,7 +55,8 @@ int main(int argc, char *argv[])
         NGOBATIN,
         LIHAT_SEMUA_ANTRIAN,
         SAVE,
-        DAFTAR_CHECKUP
+        DAFTAR_CHECKUP,
+        ANTRIAN
     };
 
     CreateCommandList(&commandList, COMMAND_READY); // Membuat List Statik yang berisikan command yang tersedia
@@ -326,6 +327,16 @@ int main(int argc, char *argv[])
             else
             {
                 printf("Akses ditolak. Fitur ini hanya dapat diakses oleh dokter.\n");
+            }
+            break;
+        case ANTRIAN:
+            if (strcmp(session.currentUser.role, "pasien") == 0)
+            {
+                LihatAntrianSaya(&userList, &session, &denahRumahSakit);
+            }
+            else
+            {
+                printf("Akses ditolak. Fitur ini hanya dapat diakses oleh pasien.\n");
             }
             break;
         default:

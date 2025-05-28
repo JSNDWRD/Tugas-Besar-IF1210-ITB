328762f1efbb294fa7ab7df9acb3c0b70212bc24
Jason Edward Salim <jasonedwardsalim@gmail.com>
2025-05-28 12:10:30 +0700

    refactor: Reset patient health data after PULANGDOK

--- src/main.c
+++ src/main.c
@@ -476,6 +476,16 @@ int main(int argc, char *argv[])
                                             printf("Pasien dengan ID %d telah keluar dari antrian.\n", idKeluar);
                                             session.currentUser.ngobatin = 0;
                                             session.currentUser.diagnosa = 0;
+                                            session.currentUser.suhuTubuh = -1;
+                                            session.currentUser.tekananDarahSistolik = -1;
+                                            session.currentUser.tekananDarahDiastolik = -1;
+                                            session.currentUser.detakJantung = -1;
+                                            session.currentUser.saturasiOksigen = -1;
+                                            session.currentUser.kadarGulaDarah = -1;
+                                            session.currentUser.beratBadan = -1;
+                                            session.currentUser.tinggiBadan = -1;
+                                            session.currentUser.kadarKolesterol = -1;
+                                            session.currentUser.trombosit = -1;
                                             break;
                                         }
                                         curr = curr->next;
